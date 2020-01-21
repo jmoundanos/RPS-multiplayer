274ae9f5755714dc7b3e5f9e19fb9aacd6da7e19
Jennifer Moundanos <jmoundanos@gmail.com>
2020-01-20 22:11:51 -0600

    finsih css started logic

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyDpmPaD1z2ugmisqrFXTW2jkMBd4XsNugQ",
+        authDomain: "rps-multiplayer-39b6b.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-39b6b.firebaseio.com",
+        projectId: "rps-multiplayer-39b6b",
+        storageBucket: "rps-multiplayer-39b6b.appspot.com",
+        messagingSenderId: "485203864969",
+        appId: "1:485203864969:web:08a3603b637430c23c4558"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    
+})
